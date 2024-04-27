75beeaafc9118f823cb30a248ad8f9cea4db7f91
vaery00 <vipul.aery@bhn.com>
2024-04-28 01:59:44 +0530

    basic project generated

---
+++ src/main/java/com/example/activitieseventprocessor/ActivitiesEventProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitieseventprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesEventProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesEventProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/activitieseventprocessor/ActivitiesEventProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activitieseventprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesEventProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
