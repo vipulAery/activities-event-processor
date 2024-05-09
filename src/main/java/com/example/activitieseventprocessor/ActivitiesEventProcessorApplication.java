ba5a69f09e504ba75bbe1277ab0e25d27921d308
vaery00 <vipul.aery@bhn.com>
2024-05-09 20:53:21 +0530

    removed commented and unused code

--- src/main/java/com/example/activitieseventprocessor/ActivitiesEventProcessorApplication.java
+++ src/main/java/com/example/activitieseventprocessor/ActivitiesEventProcessorApplication.java
@@ -9,41 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class ActivitiesEventProcessorApplication {
-//	@Autowired
-//	private Sender sender;
-////	postgres change start
-//	@Autowired
-//	UserActivityEventRepository userActivityEventRepository;
-//
-//	@Autowired
-//	UserActivityEventService userActivityEventService;
-//	@Autowired
-//	EntityManager entityManager;
-////	postgres change end
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(ActivitiesEventProcessorApplication.class, args);
 	}
-////	@PostConstruct
-//	void send() {
-//		sender.send("Spring Kafka Producer and Consumer Example");
-//
-//		//	postgres change start
-//			retrieveData();
-//		//	postgres change end
-//	}
-//
-//	//	postgres change start
-//	void retrieveData() {
-//		log.info(" Log entity manager events : " + entityManager.find(UserActivityEvent.class, "id100"));
-//		log.info(" Log all events : " + userActivityEventRepository.findAll());
-//		log.info(" Log all events : " + userActivityEventService.searchByQuery("eventId==id100"));
-//	}
-//
-//	//	postgres change end
-
-
-
 }
--- src/main/java/com/example/activitieseventprocessor/config/MessageListener.java
+++ src/main/java/com/example/activitieseventprocessor/config/MessageListener.java
@@ -3,63 +3,23 @@ package com.example.activitieseventprocessor.config;
 import com.example.activitieseventprocessor.UserActivityEvent;
 import com.example.activitieseventprocessor.dbmodule.UserActivityEventRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.protocol.Message;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Objects;
+import java.util.Random;
 
 
 @Service
 @Slf4j
 public class MessageListener {
     @Autowired
-    ExecutorService messageProcessorExecutor;
-
-//    postgres start
-    @Autowired
     UserActivityEventRepository userActivityEventRepository;
-//    postgres end
-
-
-//    private static final Logger LOG = LoggerFactory.getLogger(MessageListener.class);
-//
-//    @KafkaListener(id = "batch-listener", topics = {"${random.topic}"}, groupId = "batch-message", batch = "true")
-//    public void receive(List<Message> messages,
-//                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
-//                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
-//
-//        log.info("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-//        for (int i = 0; i < 1; i++) {
-//            log.info("received message='{}' with partition-offset='{}'",
-//                    messages.get(i), partitions.get(i) + "-" + offsets.get(i));
-//        }
-//        log.info("all the batch messages are consumed");
-//    }
-
-//    @KafkaListener(id = "single-message-listener", topics = "${random.topic}", groupId = "single-message")
-//    public void listen(List<Message> messages) {
-//        log.info("Received Messages: " + messages);
-//        // Add your custom processing logic here
-//    }
 
     @KafkaListener(id = "batch-custom-message-listener", topics = "${event.topic}", groupId = "batch-custom-message-group")
     public void listenCustomMessage(UserActivityEvent userActivityEvent) {
         log.info("Custom Message: " + userActivityEvent);
-
-//        CompletableFuture.supplyAsync(()-> userActivityEvent)
-//                .thenApplyAsync (this::processMessage, messageProcessorExecutor)
-//                .thenAccept(this::storeMessage);
 
         UserActivityEvent procesedUserActivityEvent = transformMessage(userActivityEvent);
         log.info("Custom Processed Messages: " + procesedUserActivityEvent);
